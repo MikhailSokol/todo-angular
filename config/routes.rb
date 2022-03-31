655738f7367ac66f864d4ec139593dcd0d5deab4
mike <https://github.com/MikhailSokol>
2022-04-01 01:27:10 +0300

    добавлен редирект на project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/" , to: redirect('/projects')
   get "/projects", to: "projects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
