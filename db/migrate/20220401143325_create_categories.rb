a4c260d4bbaeb38fee23bc39e954e3d657e5fc5d
mike <https://github.com/MikhailSokol>
2022-04-09 16:16:29 +0300

    del all

--- db/migrate/20220401143325_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration[7.0]
-  def change
-    create_table :categories do |t|
-      t.string :title
-      t.timestamps
-      t.string :todos , array:true
-    end
-  end
-end
